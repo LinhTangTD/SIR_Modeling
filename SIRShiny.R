f8cdc9631f8ab680b8efafdd71fc41b172f26d56
LinhTangTD <tanglinh@grinnell.edu>
2020-12-01 20:13:26 -0600

    fix basic model

--- SIRShiny.R
+++ SIRShiny.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(shinyjs)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -11,22 +12,20 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      checkboxGroupInput("Random",
-                         "Variability?",
-                         "Yes"),
       sliderInput("Pop",
                   "Population of our town",
                   min = 50,
                   max = 2000,
                   value = 500),
+      checkboxInput("WatchTilEnd", "Watch Disease till end", TRUE),
       sliderInput("Days",
-                   "Number of Days you want to see (Select -1 to see when the disease will go away)",
-                   min = -1,
+                   "Number of Days you want to see watch the disease",
+                   min = 1,
                    max = 200,
                    step = 1, 
-                   value = 50),
+                   value = 10),
       sliderInput("Initialinf",
-                   "Initial People infected",
+                  "Initial People infected",
                   min = 1,
                   max = 100,
                   step = 1, 
@@ -38,18 +37,18 @@ ui <- fluidPage(
                   step = 0.01,
                   value = 0.01),
       sliderInput("Treat_Rate",
-                  "Treatment Rate", 
+                  "Recovery Rate", 
                   min = 0.01, 
                   max = 1.00, 
                   step = 0.01,
-                  value = 0.01),
+                  value = 0.7),
       sliderInput("Susceptible_Rate",
                   "Susceptible Rate",
                   min = 0.01,
                   max = 1,
                   step = 0.01,
-                  value = 0.2)
-      
+                  value = 0.2),
+      checkboxInput("Var", "Add randomness into the model", FALSE)
     ),
     
     # Show a plot of the generated distribution
@@ -61,8 +60,18 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  basic.model = function(n = 500, beta = 0.01, gamma = 0.7, S.rate = 0.2, I = 5, day = -1){
-    
+  
+  #Dynamic Model Input 
+  observe({
+    #If regression model is not selected
+    if(input$WatchTilEnd){
+      enable("Days")
+    } else {
+      disable("Days")
+    }
+  })
+  
+  basic.model = function(n, beta, gamma, S.rate, I, day, var){
     # Calculate day 0 statistics
     curr.day = 0
     healthy = n - I
@@ -76,7 +85,7 @@ server <- function(input, output) {
     df = data_frame(Day = curr.day, Healthy = healthy, Susceptible = susceptible, Infected = infected, Recovered = recovered)
     
     # Predict S, I, R compartments by day
-    while(infected > 1){
+    while(infected*healthy > 0){
       # if number of days is specified, calculate up until that day
       if(day != -1 & curr.day >= day)
         break()
@@ -84,16 +93,19 @@ server <- function(input, output) {
       healthy = healthy - infected.next
       susceptible = round(S.rate * healthy)
       infected = infected + infected.next - treat
-      infected.next = round(infected * beta * susceptible)
-      treat = round(gamma * infected)
+      if(var){
+        infected.next = rbinom(1, infected*susceptible, beta)
+        treat = rbinom(1, infected, gamma)
+      } else {
+        infected.next = round(infected * beta * susceptible)
+        treat = round(gamma * infected)
+      }
       recovered = recovered + treat
       data = c(curr.day, healthy, susceptible, infected, recovered)
-      # data = c(curr.day, healthy, susceptible, infected, infected.next, treat, recovered)
       df = rbind(df, data)
     }
     return(df)
   }
-  
   
   model.plot = function(model){
     ggplot(data  = model, aes(x = Day)) +
@@ -104,52 +116,26 @@ server <- function(input, output) {
       labs(y = "Population", title = "Basic SIR Model") + 
       theme_minimal()
   }
-  
-  basic.model.rand = function(n = 500, beta = 0.01, gamma = 0.7, S.rate = 0.2, I = 5, t = 50){
-    curr.day = 0
-    healthy = n - I
-    susceptible = round(S.rate * healthy)
-    infected = I
-    infected.next = round(I * beta * susceptible)
-    treat = round(gamma * I)
-    recovered = 0
-    
-    df = data_frame(Day = curr.day, Healthy = healthy, Susceptible = susceptible, Infected = infected, Recovered = recovered)
-    # df = data_frame(Day = curr.day, Healthy = healthy, Susceptible = susceptible, Infected = infected, I.next = infected.next, Tr = treat, R = recovered)
-    
-    for(i in 1:t){
-      if(infected == 0)
-        break()
-      curr.day = curr.day + 1
-      healthy = healthy - infected.next
-      susceptible = round(S.rate * healthy)
-      infected = infected + infected.next - treat
-      infected.next = rbinom(1, infected*susceptible, beta) # I introduced an rbinom command here which give a random outcome based on the binomial distribtuion of infected*susceptible size and a success rate of beta.
-      treat = rbinom(1, infected, gamma) # Here is the other rbinom command here which give a random outcome based on the binomial distribtuion with infected size and a success rate of beta.
-      recovered = recovered + treat
-      data = c(curr.day, healthy, susceptible, infected, recovered)
-      # data = c(curr.day, healthy, susceptible, infected, infected.next, treat, recovered)
-      df = rbind(df, data)
-    }
-    return(df)
-  }
 
   output$distPlot <- renderPlot({
     validate(
-      need(input$Initialinf < input$Pop, "Initial Infected Cannot be more than the population")
+      need(input$Initialinf < input$Pop, "Initial infected number cannot be more than the population")
     )
-
-    if("Yes" %in% input$Random){
-    df = basic.model.rand(n = input$Pop, beta = input$Spread_Rate, gamma = input$Treat_Rate, S.rate = input$Susceptible_Rate, I = input$Initialinf, t = input$Days)
-    model.plot(df)   
+    if(input$WatchTilEnd){
+      day = -1
+    } else {
+      day = input$Days
     }
-    else{
-    df = basic.model(n = input$Pop, beta = input$Spread_Rate, gamma = input$Treat_Rate, S.rate = input$Susceptible_Rate, I = input$Initialinf, day = input$Days)
+    df = basic.model(n = input$Pop, 
+                          beta = input$Spread_Rate, 
+                          gamma = input$Treat_Rate, 
+                          S.rate = input$Susceptible_Rate, 
+                          I = input$Initialinf, 
+                          day = day,
+                          var = input$Var)
     model.plot(df)
- }
  })
 }
-
 
 # Run the application 
 shinyApp(ui = ui, server = server)
